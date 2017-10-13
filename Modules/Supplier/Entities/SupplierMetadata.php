01b6349d1b08a1a2747304dd7f4053d416e799fc
Arya Pujianto <aryapujianto2@gmail.com>
2017-10-13 15:44:11 +0700

    commit belum solve

---
+++ Modules/Supplier/Entities/SupplierMetadata.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Supplier\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierMetadata extends Model
+{
+    protected $fillable = [];
+    protected $table = 'supplier_metadata';
+}

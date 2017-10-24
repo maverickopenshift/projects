fd3b5baba4200d7e4345bc58983ac9b7d93f2055
Arya Pujianto <aryapujianto@inarts.co.id>
2017-10-25 02:27:06 +0700

    fix supplier

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>{{ env('APP_NAME') }} {{ $page_title or null }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+    <link href="{{ mix('css/all.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>;
+    </script>
+</head>
+<body class="skin-red-light">
+<div class="wrapper">
+
+    <!-- Header -->
+    @include('layouts.header')
+
+    <!-- Sidebar -->
+    @include('layouts.sidebar')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <h1>Unauthorized action.</h1>
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+    @include('layouts.footer')
+</div><!-- ./wrapper -->
+
+
+<script src="{{ mix('js/all.js') }}"></script>
+@stack('scripts')
+</body>
+</html>

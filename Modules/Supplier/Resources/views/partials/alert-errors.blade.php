10afe4488fe11dbdbfd540e7649a7c6cafd99bbe
Arya Pujianto <aryapujianto@inarts.co.id>
2017-10-25 02:41:38 +0700

    delete

--- Modules/Supplier/Resources/views/partials/alert-errors.blade.php
+++
@@ -1,6 +0,0 @@
-@if ($errors->any())
-<div class="alert alert-danger alert-dismissible">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-  Data yang Anda masukan belum valid, silahkan periksa kembali!
-</div>
-@endif

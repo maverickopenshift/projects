bb56c81a5da65634aa7d831edd741dd51fc0b849
Anisa <nurul@inarts.co.id>
2017-10-31 13:38:03 +0700

    dok

--- Modules/Documents/Http/routes.php
+++ Modules/Documents/Http/routes.php
@@ -4,8 +4,10 @@ Route::group(['middleware' => ['web','auth'], 'prefix' => 'documents', 'namespac
 {
     Route::get('/', 'DocumentsController@index')->name('doc');
     Route::get('/create/{type}', 'EntryDocumentController@index')->name('doc.create');
-    
+
     Route::get('/doc-template', 'DocTemplateController@index')->name('doc.template');
     Route::get('/doc-template/create', 'DocTemplateController@create')->name('doc.template.create');
     Route::post('/doc-template/store', 'DocTemplateController@store')->name('doc.template.store');
+
+    Route::get('/doc-create-contract', 'CreatekontrakController@index')->name('doc.template.create.contract');
 });

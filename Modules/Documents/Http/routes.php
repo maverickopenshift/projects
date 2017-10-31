e5bbea9c2bb0c89a1421d70715344d339dece120
Anisa <nurul@inarts.co.id>
2017-10-31 15:46:30 +0700

    doku

--- Modules/Documents/Http/routes.php
+++ Modules/Documents/Http/routes.php
@@ -4,7 +4,7 @@ Route::group(['middleware' => ['web','auth'], 'prefix' => 'documents', 'namespac
 {
     Route::get('/', 'DocumentsController@index')->name('doc');
     Route::get('/create/{type}', 'EntryDocumentController@index')->name('doc.create');
-    
+
     Route::get('/doc-template', 'DocTemplateController@index')->name('doc.template');
     Route::get('/doc-template/create', 'DocTemplateController@create')->name('doc.template.create');
     Route::post('/doc-template/store', 'DocTemplateController@store')->name('doc.template.store');

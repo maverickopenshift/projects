74fd034490b1209cacc80b8f7581db3b8ebe54cd
Arya Pujianto <aryapujianto@inarts.co.id>
2017-11-09 00:25:21 +0700

    set welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 <div class="box box-success">
     <div class="box-header with-border">
-      <h3 class="box-title">Visitors Report</h3>
-
-      <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-        </button>
-        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-      </div>
+      <h3 class="box-title">Welcome</h3>
     </div>
     <!-- /.box-header -->
     <div class="box-body">
@@ -23,7 +17,7 @@
     </div>
 <!-- /.box-body -->
 </div>
-<div class="box box-danger">
+{{-- <div class="box box-danger">
     <div class="box-header with-border">
       <h3 class="box-title">Box Danger</h3>
 
@@ -38,5 +32,5 @@
         Test Box danger
     </div>
 <!-- /.box-body -->
-</div>
+</div> --}}
 @endsection

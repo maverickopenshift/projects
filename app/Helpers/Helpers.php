b6f14c3178510f5748f19da1d7ef216de7c69e7f
irfan <irfan@inarts.co.id>
2017-11-30 18:01:24 +0700

    helper status

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -329,7 +329,7 @@ public static function select_atasan($pegawai,$val=null)
         $ret ='<span class="label label-danger">return</span>';
       }
       else{
-        $ret ='<span class="label label-info">proses</span>';
+        $ret ='<span class="label label-info">in-progress</span>';
         // if($status==1)//reject
         // {
         //   $ret ='<button type="button" class="btn btn-xs btn-danger btn-reject" data-msg="'.$message.'">reject</button>';

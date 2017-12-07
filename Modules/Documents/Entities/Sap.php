7042b8381fa5fd02f0466f1e16d22ce66d840310
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-07 22:33:39 +0700

    po sap with soap

--- Modules/Documents/Entities/Sap.php
+++ Modules/Documents/Entities/Sap.php
@@ -3,9 +3,8 @@
 namespace Modules\Documents\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-
-use SAPNWRFC\Connection as SapConnection;
-use SAPNWRFC\Exception as SapException;
+use SoapClient;
+use SoapFault;
 
 class Sap extends Model
 {
@@ -13,15 +12,20 @@ class Sap extends Model
     
     public static function get_po($po){
       try {
-          $c = new SapConnection(config('app.sap'));
-          $f = $c->getFunction('Z_MM_PRPO');
-          $result = $f->invoke([
-              'EBELN' => $po
-          ]);
-          return ['status'=>true,'data'=>$result,'length'=>count($result['POITEM'])];
-      } catch(SapException $ex) {
-          $msg = 'Exception: ' . $ex->getMessage() . PHP_EOL;
-          return ['status'=>false,'data'=>$msg,'length'=>0];
+        $client = new SoapClient(config('app.soap_sap_po'), array('login' => config('app.sap.user'),'password' => config('app.sap.passwd')));
+        $get_po = $client->ZMmPrpo([
+                  'Ebeln' => $po,
+                  'Banfn' => false,
+                  'Poheader' => false,
+                  'Poitem' => false,
+                  'Pritem'=>false,
+                  'UsrInf'=>false,
+                  'VendorInf'=>false
+                ]);
+          return ['status'=>true,'data'=>$get_po,'length'=>count($get_po->Poitem->item)];
+      } catch(SoapFault $ex) {
+          //$msg = 'Exception: ' . $ex->getMessage() . PHP_EOL;
+          return ['status'=>true,'data'=>$ex->faultstring,'other'=>$ex,'length'=>0];
       }
     }
 }

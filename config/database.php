400a630e3a8c07512f29a10a322220f1fe6e3af0
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 05:11:00 +0700

    oke

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', getenv('DATABASE_SERVICE_NAME')),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', getenv('MYSQL_DATABASE')),
             'username' => env('DB_USERNAME', getenv('MYSQL_USER')),

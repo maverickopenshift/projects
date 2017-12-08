412c96a386e1640f1c8aa4df4a163dbdb80bb3a3
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 01:40:44 +0700

    oke

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', getenv('DATABASE_SERVICE_NAME')),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', getenv('MYSQL_DATABASE')),
+            'username' => env('DB_USERNAME', getenv('MYSQL_USER')),
+            'password' => env('DB_PASSWORD', getenv('MYSQL_PASSWORD')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

943ab57ea80674a20b44927975397cd4a5c24e69
Anisa <nurul@inarts.co.id>
2017-12-28 17:24:20 +0700

    benerinbugtombol

--- Modules/Supplier/Resources/views/sap/mapping_sap.blade.php
+++ Modules/Supplier/Resources/views/sap/mapping_sap.blade.php
@@ -54,7 +54,7 @@
         </table>
         <div class="form-group text-center top50 btn_smpn">
           <a href="{{route('supplier')}}" class="btn btn-warning" style="padding:5px 20px;font-weight:bold;font-size:16px;margin-right:10px">BACK</a>
-          @if(count($sap)>1)
+          @if(count($sap)>0)
           <button type="submit" class="btn btn-success btn-sbm" style="padding:5px 20px;font-weight:bold;font-size:16px;">SIMPAN</button>
         @endif
         </div>
